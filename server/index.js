ed42fb8b1229ee5eab72d6c0c57e28780fa6c8d9
Kyle Cuss <cussprogramming@gmail.com>
2022-10-29 08:43:07 -0230

    Frontend structure and styling implemented

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use('/posts', postRoutes);
 
 //middleware for bodyparser to send requests
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 //cors middleware
 app.use(cors());
 
