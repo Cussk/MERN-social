8d2607eceb1d340c7ceb803b0820afb94ecf19ed
Kyle Cuss <cussprogramming@gmail.com>
2022-10-30 17:38:14 -0230

    created form for making posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import { useDispatch } from "react-redux";
 import {getPosts} from './actions/posts';
 import memories from './images/memories.png';
@@ -26,7 +26,7 @@ useEffect(() => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justifycontained="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
